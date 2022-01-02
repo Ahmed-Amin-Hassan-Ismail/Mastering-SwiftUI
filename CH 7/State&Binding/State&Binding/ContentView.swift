2f0b8655bf1af4d3ace0a8b57973a7101bd2ae65
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-02 23:22:12 +0200

    Chapter 7 part 2 -> Add binding between subView and superView

--- CH 7/State&Binding/State&Binding/ContentView.swift
+++ CH 7/State&Binding/State&Binding/ContentView.swift
@@ -10,34 +10,34 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var isPlaying: Bool = false
-    @State private var pressCounter: Int = 0
+    @State private var blueCounter: Int = 0
+    @State private var greenCounter: Int = 0
+    @State private var redCounter: Int = 0
     
     var body: some View {
         
+        
+        
         VStack {
-            Button {
-                print("Play button is clicked")
-                self.isPlaying.toggle()
-                
-            } label: {
-                Image(systemName: isPlaying ?  "stop.circle.fill" : "play.circle.fill")
-                    .font(.system(size: 150))
-                    .foregroundColor(isPlaying ?  .red : .green)
-        }
+            Spacer()
             
-            Button {
-                print("Counter is increased")
-                self.pressCounter += 1
-            } label: {
-                Circle()
-                    .frame(width: 150, height: 150, alignment: .center)
-                    .foregroundColor(.red)
-                    .overlay(
-                        Text("\(pressCounter)")
-                            .font(.system(size: 100, weight: .bold, design: .rounded))
-                            .foregroundColor(.white)
-                    )
+            Text("\(blueCounter + greenCounter + redCounter)")
+                .font(.system(size: 150, weight: .bold, design: .rounded))
+                .foregroundColor(.black)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                CircularButton(colorSelected: .blue, counter: $blueCounter)
+                CircularButton(colorSelected: .green, counter: $greenCounter)
+                CircularButton(colorSelected: .red, counter: $redCounter)
             }
+            
+            Spacer()
+           
+
+
 
         }
 
@@ -47,5 +47,28 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CircularButton: View {
+    
+    var colorSelected: Color
+    @Binding var counter: Int
+    
+    
+    var body: some View {
+        Button {
+            print("The \(colorSelected.description) button is tapped ")
+            self.counter += 1
+        } label: {
+            Circle()
+                .frame(width: 120, height: 120, alignment: .center)
+                .foregroundColor(colorSelected)
+                .overlay(
+                    Text("\(self.counter)")
+                        .font(.system(size: 70, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+                )
+        }
     }
 }
