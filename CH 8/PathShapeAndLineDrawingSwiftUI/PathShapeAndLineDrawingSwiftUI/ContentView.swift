cda647f1a1fad171c5544b2c7920281eae8ff1bc
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-05 00:27:47 +0200

    add button with flexible shape using Shape Protocol

--- CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
+++ CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
@@ -9,71 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Path() { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200),
-                            radius: 150,
-                            startAngle: Angle(degrees: 0.0),
-                            endAngle: Angle(degrees: 190),
-                            clockwise: true)
-            }
-            .fill(.green)
-            
-            Path() { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200),
-                            radius: 150,
-                            startAngle: Angle(degrees: 190),
-                            endAngle: Angle(degrees: 110),
-                            clockwise: true)
-            }
-            .fill(Color(.systemTeal))
-            
-            Path() { path in
-                path.move(to: CGPoint(x: 200, y: 200))
-                path.addArc(center: CGPoint(x: 200, y: 200),
-                            radius: 150,
-                            startAngle: Angle(degrees: 110),
-                            endAngle: Angle(degrees: 90),
-                            clockwise: true)
-            }
-            .fill(.blue)
-            
-            
-            ZStack {
-                Path() { path in
-                    path.move(to: CGPoint(x: 200, y: 200))
-                    path.addArc(center: CGPoint(x: 200, y: 200),
-                                radius: 150,
-                                startAngle: Angle(degrees: 90),
-                                endAngle: Angle(degrees: 360),
-                                clockwise: true)
-                }
-                .fill(.purple)
-                .offset(x: 20, y: 20)
-                
-                Path() { path in
-                    path.move(to: CGPoint(x: 200, y: 200))
-                    path.addArc(center: CGPoint(x: 200, y: 200),
-                                radius: 150,
-                                startAngle: Angle(degrees: 90),
-                                endAngle: Angle(degrees: 360),
-                                clockwise: true)
-                    path.closeSubpath()
-                }
-                .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
-                .offset(x: 20, y: 20)
-                .overlay(
-                    Text("%25")
-                        .font(.system(.largeTitle, design: .rounded))
-                        .bold()
-                        .foregroundColor(.white)
-                        .offset(x: 80, y: -120)
-                )
-            }
-            }
         
+        Button {
+            
+        } label: {
+            Text("Hit Me")
+                .font(.system(.title, design: .rounded))
+                .bold()
+                .foregroundColor(.white)
+                .frame(width: 250, height: 50, alignment: .center)
+                .background(Demo().fill(.red))
+        }
+
     }
 }
 
@@ -81,4 +28,22 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Demo: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addQuadCurve(to: CGPoint(x: rect.size.width,
+                                      y: 0),
+                          control: CGPoint(x: rect.size.width / 2,
+                                           y: -(rect.size.height) / 2))
+        path.addRect(CGRect(x: 0, y: 0, width: rect.size.width,
+                            height: rect.size.height))
+        
+        return path
+    }
+    
+    
 }
