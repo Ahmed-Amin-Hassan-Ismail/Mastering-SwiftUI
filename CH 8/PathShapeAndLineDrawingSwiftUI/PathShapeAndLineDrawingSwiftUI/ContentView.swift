4cc6628a39e0dd39d783059ebca69c156f3c040d
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-05 01:08:36 +0200

    Drawing a Donut Chart

--- CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
+++ CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
@@ -13,27 +13,33 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-            Circle()
-                .stroke(Color(.systemGray6), lineWidth: 6)
-                .frame(width: 300, height: 300, alignment: .center)
             
             Circle()
-                .trim(from: 0, to: 0.85)
-                .stroke(purpuleGradient, lineWidth: 20)
-                .frame(width: 300, height: 300, alignment: .center)
+                .trim(from: 0, to: 0.4)
+                .stroke(Color(.systemBlue), lineWidth: 80)
+            
+            Circle()
+                .trim(from: 0.4, to: 0.6)
+                .stroke(Color(.systemTeal), lineWidth: 80)
+            
+            Circle()
+                .trim(from: 0.6, to: 0.75)
+                .stroke(Color(.systemPurple), lineWidth: 80)
+            
+            Circle()
+                .trim(from: 0.75, to: 1)
+                .stroke(Color(.systemYellow), lineWidth: 90)
+            
                 .overlay(
-                    VStack {
-                        Text("%85")
-                            .font(.system(size: 80, weight: .bold, design: .rounded))
-                            .foregroundColor(Color(.systemGray))
-                        Text("Completed")
-                            .font(.system(.body, design: .rounded))
-                            .bold()
-                            .foregroundColor(Color(.systemGray))
-                                    
-                    }
+                    Text("%25")
+                        .font(.system(.title, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                        .offset(x: 120, y: -100)
+                    
                 )
         }
+        .frame(width: 300, height: 300, alignment: .center)
         
         
         
@@ -45,22 +51,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-}
-
-struct Demo: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: 0, y: 0))
-        path.addQuadCurve(to: CGPoint(x: rect.size.width,
-                                      y: 0),
-                          control: CGPoint(x: rect.size.width / 2,
-                                           y: -(rect.size.height) / 2))
-        path.addRect(CGRect(x: 0, y: 0, width: rect.size.width,
-                            height: rect.size.height))
-        
-        return path
-    }
-    
-    
 }
