2a1981ae595bde12d13aaa1f0084a76ca5e1cc66
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-05 01:00:37 +0200

    Creating a Progress Indicator Using Shapes

--- CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
+++ CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
@@ -8,19 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var purpuleGradient = LinearGradient(gradient: Gradient(colors: [Color(red: 207/255, green: 150/255, blue: 207/255), Color(red: 107/255, green: 116/255, blue: 179/255) ]), startPoint: .trailing, endPoint: .leading)
+    
     var body: some View {
         
-        Button {
+        ZStack {
+            Circle()
+                .stroke(Color(.systemGray6), lineWidth: 6)
+                .frame(width: 300, height: 300, alignment: .center)
             
-        } label: {
-            Text("Hit Me")
-                .font(.system(.title, design: .rounded))
-                .bold()
-                .foregroundColor(.white)
-                .frame(width: 250, height: 50, alignment: .center)
-                .background(Demo().fill(.red))
+            Circle()
+                .trim(from: 0, to: 0.85)
+                .stroke(purpuleGradient, lineWidth: 20)
+                .frame(width: 300, height: 300, alignment: .center)
+                .overlay(
+                    VStack {
+                        Text("%85")
+                            .font(.system(size: 80, weight: .bold, design: .rounded))
+                            .foregroundColor(Color(.systemGray))
+                        Text("Completed")
+                            .font(.system(.body, design: .rounded))
+                            .bold()
+                            .foregroundColor(Color(.systemGray))
+                                    
+                    }
+                )
         }
-
+        
+        
+        
+        
     }
 }
 
