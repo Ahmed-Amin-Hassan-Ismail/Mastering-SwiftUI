b3386892050dc4053b1ade731797ebf74413cf3e
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-05 00:14:00 +0200

    Drawing Arcs and Pie Charts

--- CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
+++ CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
@@ -9,42 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
             Path() { path in
-                path.move(to: CGPoint(x: 20, y: 20))
-                path.addLine(to: CGPoint(x: 300, y: 20))
-                path.addLine(to: CGPoint(x: 300, y: 200))
-                path.addLine(to: CGPoint(x: 20, y: 200))
-                path.closeSubpath()
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200),
+                            radius: 150,
+                            startAngle: Angle(degrees: 0.0),
+                            endAngle: Angle(degrees: 190),
+                            clockwise: true)
             }
-            .stroke(.green, lineWidth: 5)
+            .fill(.green)
             
-            // Draw shape with border width using zstak
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200),
+                            radius: 150,
+                            startAngle: Angle(degrees: 190),
+                            endAngle: Angle(degrees: 110),
+                            clockwise: true)
+            }
+            .fill(Color(.systemTeal))
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 200, y: 200))
+                path.addArc(center: CGPoint(x: 200, y: 200),
+                            radius: 150,
+                            startAngle: Angle(degrees: 110),
+                            endAngle: Angle(degrees: 90),
+                            clockwise: true)
+            }
+            .fill(.blue)
+            
+            
             ZStack {
-
                 Path() { path in
-                    path.move(to: CGPoint(x: 20, y: 60))
-                    path.addLine(to: CGPoint(x: 40, y: 60))
-                    path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
-                    path.addLine(to: CGPoint(x: 230, y: 60))
-                    path.addLine(to: CGPoint(x: 230, y: 100))
-                    path.addLine(to: CGPoint(x: 20, y: 100))
+                    path.move(to: CGPoint(x: 200, y: 200))
+                    path.addArc(center: CGPoint(x: 200, y: 200),
+                                radius: 150,
+                                startAngle: Angle(degrees: 90),
+                                endAngle: Angle(degrees: 360),
+                                clockwise: true)
                 }
                 .fill(.purple)
-
+                .offset(x: 20, y: 20)
+                
                 Path() { path in
-                    path.move(to: CGPoint(x: 20, y: 60))
-                    path.addLine(to: CGPoint(x: 40, y: 60))
-                    path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
-                    path.addLine(to: CGPoint(x: 230, y: 60))
-                    path.addLine(to: CGPoint(x: 230, y: 100))
-                    path.addLine(to: CGPoint(x: 20, y: 100))
+                    path.move(to: CGPoint(x: 200, y: 200))
+                    path.addArc(center: CGPoint(x: 200, y: 200),
+                                radius: 150,
+                                startAngle: Angle(degrees: 90),
+                                endAngle: Angle(degrees: 360),
+                                clockwise: true)
                     path.closeSubpath()
                 }
-                .stroke(.green, lineWidth: 5)
+                .stroke(Color(red: 52/255, green: 52/255, blue: 122/255), lineWidth: 10)
+                .offset(x: 20, y: 20)
+                .overlay(
+                    Text("%25")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                        .offset(x: 80, y: -120)
+                )
             }
-            
-        }
+            }
+        
     }
 }
 
