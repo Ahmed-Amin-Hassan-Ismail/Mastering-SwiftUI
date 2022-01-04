f22d29f2cb780f340df28f9521cf8660dc6614ac
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-04 23:59:44 +0200

    add shape with QuadCurve and border width

--- CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
+++ CH 8/PathShapeAndLineDrawingSwiftUI/PathShapeAndLineDrawingSwiftUI/ContentView.swift
@@ -9,16 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Path() { path in
-            path.move(to: CGPoint(x: 20, y: 20))
-            path.addLine(to: CGPoint(x: 300, y: 20))
-            path.addLine(to: CGPoint(x: 300, y: 200))
-            path.addLine(to: CGPoint(x: 20, y: 200))
-            path.closeSubpath()
+        VStack {
+            Path() { path in
+                path.move(to: CGPoint(x: 20, y: 20))
+                path.addLine(to: CGPoint(x: 300, y: 20))
+                path.addLine(to: CGPoint(x: 300, y: 200))
+                path.addLine(to: CGPoint(x: 20, y: 200))
+                path.closeSubpath()
+            }
+            .stroke(.green, lineWidth: 5)
+            
+            // Draw shape with border width using zstak
+            ZStack {
+
+                Path() { path in
+                    path.move(to: CGPoint(x: 20, y: 60))
+                    path.addLine(to: CGPoint(x: 40, y: 60))
+                    path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
+                    path.addLine(to: CGPoint(x: 230, y: 60))
+                    path.addLine(to: CGPoint(x: 230, y: 100))
+                    path.addLine(to: CGPoint(x: 20, y: 100))
+                }
+                .fill(.purple)
+
+                Path() { path in
+                    path.move(to: CGPoint(x: 20, y: 60))
+                    path.addLine(to: CGPoint(x: 40, y: 60))
+                    path.addQuadCurve(to: CGPoint(x: 210, y: 60), control: CGPoint(x: 125, y: 0))
+                    path.addLine(to: CGPoint(x: 230, y: 60))
+                    path.addLine(to: CGPoint(x: 230, y: 100))
+                    path.addLine(to: CGPoint(x: 20, y: 100))
+                    path.closeSubpath()
+                }
+                .stroke(.green, lineWidth: 5)
+            }
+            
         }
-        //.fill(Color.green)
-        
-        .stroke(.green, lineWidth: 5)
     }
 }
 
