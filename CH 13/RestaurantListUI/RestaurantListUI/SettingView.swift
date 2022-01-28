95e3d7a555e40e4faecbdc23236b5396ea2fab93
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-28 17:56:50 +0200

    create Setting UI View

---
+++ CH 13/RestaurantListUI/RestaurantListUI/SettingView.swift
@@ -0,0 +1,71 @@
+//
+//  SettingView.swift
+//  RestaurantListUI
+//
+//  Created by Ahmed Amin on 28/01/2022.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    
+    @State private var selectedOrder = 0
+    @State private var maxPriceLevel = 5
+    @State private var showCheckIn = false
+    
+    private var displayOrders = [ "Alphabetical", "Show Favorite First", "Show Check-in First"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker(selection: $selectedOrder) {
+                        ForEach(0..<displayOrders.count, id: \.self) {
+                            Text(self.displayOrders[$0])
+                        }
+                    } label: {
+                        Text("Display Order")
+                    }
+                } header: {
+                    Text("SORT PREFERENCE")
+                }
+                
+                
+                Section {
+                    Toggle(isOn: $showCheckIn) {
+                        Text("Show Check-in Only")
+                    }
+                    
+                    Stepper {
+                        Text("Show \(String(repeating: "$", count: maxPriceLevel)) or below")
+                    } onIncrement: {
+                        self.maxPriceLevel += 1
+                        
+                        if self.maxPriceLevel >= 5 {
+                            self.maxPriceLevel = 5
+                        }
+                    } onDecrement: {
+                        self.maxPriceLevel -= 1
+                        
+                        if self.maxPriceLevel <= 1 {
+                            self.maxPriceLevel = 1
+                        }
+                    }
+                    
+                    
+                    
+                } header: {
+                    Text("FILTER PREFERENCE")
+                }
+                
+                .navigationTitle("Settings")
+            }
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
