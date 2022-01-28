940462152eeff2987d33e1c2031c99d344269d1a
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-28 16:40:58 +0200

    initial code

---
+++ CH 13/RestaurantListUI/RestaurantListUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestaurantListUI
+//
+//  Created by Ahmed Amin on 28/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CH 13/RestaurantListUI/RestaurantListUI/RestaurantListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantListUIApp.swift
+//  RestaurantListUI
+//
+//  Created by Ahmed Amin on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
