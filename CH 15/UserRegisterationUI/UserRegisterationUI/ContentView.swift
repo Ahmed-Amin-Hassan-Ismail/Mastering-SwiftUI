e07ad984c80f9336d22b89156931cbc3d32de8d8
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-01-29 23:09:59 +0200

    Initial commit chapter 15

---
+++ CH 15/UserRegisterationUI/UserRegisterationUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserRegisterationUI
+//
+//  Created by Ahmed Amin on 29/01/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CH 15/UserRegisterationUI/UserRegisterationUI/UserRegisterationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserRegisterationUIApp.swift
+//  UserRegisterationUI
+//
+//  Created by Ahmed Amin on 29/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct UserRegisterationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
