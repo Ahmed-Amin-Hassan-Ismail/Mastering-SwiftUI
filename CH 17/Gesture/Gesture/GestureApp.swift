708f4783c94f34158c60dd3787e9e59a0b21acff
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-03-17 17:06:18 +0200

    initial gesture chapter

---
+++ CH 17/Gesture/Gesture/GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureApp.swift
+//  Gesture
+//
+//  Created by Ahmed Amin on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
