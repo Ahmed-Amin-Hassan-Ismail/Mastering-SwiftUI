41fce0567eaf933c08aee1625744c9e9aed8f859
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-03-18 16:36:53 +0200

    add @GestureState property wrapper

--- CH 17/Gesture/Gesture/ContentView.swift
+++ CH 17/Gesture/Gesture/ContentView.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isPressed: Bool = false
+    @GestureState private var longPressTap = false
     
     var body: some View {
        Image(systemName: "star.circle.fill")
             .font(.system(size: 200))
-            .foregroundColor(.green)
+            .opacity(longPressTap ? 0.4 : 1.0)
             .scaleEffect(isPressed ? 0.5 : 1.0)
+            .animation(.easeInOut)
+            .foregroundColor(.green)
             .gesture(
                 LongPressGesture(minimumDuration: 1.0)
+                    .updating($longPressTap, body: { currentState, state, transaction in
+                        state = currentState
+                    })
                     .onEnded({ _ in
                         self.isPressed.toggle()
                     })
