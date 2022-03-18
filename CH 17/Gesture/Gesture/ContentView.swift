535e750ae450b47b9966c9d12ae8a0c528307309
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-03-18 17:45:12 +0200

    create complex gesture and refactor the code

--- CH 17/Gesture/Gesture/ContentView.swift
+++ CH 17/Gesture/Gesture/ContentView.swift
@@ -7,28 +7,66 @@
 
 import SwiftUI
 
+
+enum DragState {
+    case inactive
+    case pressing
+    case dragging(translation: CGSize)
+    
+    var translation: CGSize {
+        switch self {
+        case .inactive, .pressing:
+            return .zero
+        case .dragging(let translation):
+            return translation
+        }
+    }
+    
+    var isPressing: Bool {
+        switch self {
+        case .pressing, .dragging:
+                return true
+        case .inactive:
+            return false
+        }
+    }
+}
+
 struct ContentView: View {
-    @State private var isPressed: Bool = false
-    @GestureState private var dragOffset = CGSize.zero
+    @GestureState private var dragState = DragState.inactive
     @State private var position = CGSize.zero
     
     var body: some View {
        Image(systemName: "star.circle.fill")
-            .font(.system(size: 200))
-            .offset(x: position.width + dragOffset.width,
-                    y: position.height + dragOffset.height)
+            .font(.system(size: 100))
+            .opacity(dragState.isPressing ? 0.5 : 1.0)
+            .offset(x: position.width + dragState.translation.width,
+                    y: position.height + dragState.translation.height)
             .animation(.easeInOut)
             .foregroundColor(.green)
-        
             .gesture(
-                DragGesture()
-                    .updating($dragOffset, body: { value, state, transaction in
-                        state = value.translation
+                LongPressGesture(minimumDuration: 1.0)
+                    .sequenced(before: DragGesture())
+                    .updating($dragState, body: { value, state, transaction in
+                        switch value {
+                        case .first(true):
+                            state = .pressing
+                        case .second(true, let drag):
+                            state = .dragging(translation: drag?.translation ?? .zero)
+                        default:
+                            break
+                        }
                     })
+                
                     .onEnded({ value in
-                        self.position.height += value.translation.height
-                        self.position.width += value.translation.width
+                        guard case .second(true, let drag?) = value else {
+                            return
+                        }
+                        self.position.height = drag.translation.height
+                        self.position.width = drag.translation.width
                     })
+                
+    
             )
     }
 }
