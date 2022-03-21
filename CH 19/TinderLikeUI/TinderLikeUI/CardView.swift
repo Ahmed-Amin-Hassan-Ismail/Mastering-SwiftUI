3f0bc6c68842de03512971f1c10b32aaf4dad9b1
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-03-21 15:23:51 +0200

    add card view

--- CH 19/TinderLikeUI/TinderLikeUI/CardView.swift
+++ CH 19/TinderLikeUI/TinderLikeUI/CardView.swift
@@ -7,14 +7,45 @@
 
 import SwiftUI
 
-struct CardView: View {
+struct CardView: View, Identifiable {
+    var id = UUID()
+    var image: String
+    var title: String
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+            Image(image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, idealWidth: nil, maxWidth: .infinity, minHeight: nil, idealHeight: nil, maxHeight: nil, alignment: .center)
+                .cornerRadius(10)
+                .padding(.horizontal, 15)
+                .overlay(
+                    VStack {
+                        Text(title)
+                            .font(.system(.headline, design: .rounded))
+                            .bold()
+                            .padding(.horizontal, 30)
+                            .padding(.vertical, 10)
+                            .background(.white)
+                        .cornerRadius(5)
+                    }
+                        .padding(.bottom, 20)
+                    ,alignment: .bottom
+                )
+            
+            
+                
+                
+   
+                
+            
+            
+        
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(image: "barcelona-spain", title: "Mount Currie, Canada")
     }
 }
