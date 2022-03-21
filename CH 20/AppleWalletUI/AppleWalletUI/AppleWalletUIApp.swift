23d0e0ecf6af351431cf679d0bd2a74564d8d8d2
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-03-21 23:34:13 +0200

    create Apple wallet Project and asset their images

---
+++ CH 20/AppleWalletUI/AppleWalletUI/AppleWalletUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleWalletUIApp.swift
+//  AppleWalletUI
+//
+//  Created by Ahmed Amin on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleWalletUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
