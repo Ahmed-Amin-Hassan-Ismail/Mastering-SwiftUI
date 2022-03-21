4721a5200d65c936b3ff419fcc0f0fe3bd03a0ba
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-03-21 15:05:08 +0200

    create project init , add assets

---
+++ CH 19/TinderLikeUI/TinderLikeUI/TinderLikeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderLikeUIApp.swift
+//  TinderLikeUI
+//
+//  Created by Ahmed Amin on 21/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TinderLikeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
